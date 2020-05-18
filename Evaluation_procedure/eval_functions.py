41d89f1aad8bec5ca09218cab3f1a537f81fa874
Ben Snow <56250881+BenSnow6@users.noreply.github.com>
2020-05-18 17:19:53 +0100

    refactoring and kitti dataloader

--- Evaluation_procedure/eval_functions.py
+++ Evaluation_procedure/eval_functions.py
@@ -4,6 +4,25 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import math
 import sys
+from tqdm.notebook import trange, tqdm
+import torch
+import torchvision
+import pandas as pd
+import os
+from os import walk
+from skimage import io, transform
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import warnings
+warnings.filterwarnings("ignore")
+from pathlib import Path
+from os import listdir
+from os.path import isfile, join
+import torch.nn.functional as F
+from torchsummary import summary
+import math
+
+
 
 def get_depth(depth_img,u,v):
     return depth_img[u][v]
@@ -93,3 +112,153 @@ def calc_errors(pred_depth, grndt_depth):
     else:
         raise MyError('Depth maps do not have the same dimensions!')
     return [difference_err, sqr_diff_err, inv_err, inv_sqr_err, log_err, log_sqr_err, scale_inv_err, abs_rel_err, sqr_rel_err]
+
+def predict_and_gt(_dl, _val_size, _batch_size, _model):
+
+    count_test_batches = 0
+    chunk_size = _val_size//_batch_size
+    for xb, yb in tqdm(_dl, leave=False):
+        if count_test_batches == 0:
+            # initialise list of predicted depths
+            numpy_depth_prediction = [None]*(chunk_size+1)
+            gt_depths = [None]*(chunk_size+1)
+            for i in range(0, (chunk_size+1)):
+                numpy_depth_prediction[i] = [None]*xb.shape[0]
+                gt_depths[i] = [None]*xb.shape[0]
+        # transform data into floats and then place on gpu
+        xb, yb = xb.float(), yb.float()
+        xb, yb = xb.cuda(), yb.cuda()
+
+        # run the x's through the trained network to predict their depth maps
+        prediction = _model(xb)
+        
+        # save each predicted depth map in the batch to a list as a numpy array
+        for i in range(0, xb.shape[0]):
+            # reshape the predictions to the correct size:(720,1280) from (1,720,1280)
+            numpy_depth_prediction[count_test_batches][i] = np.reshape(prediction[i].cpu().detach().numpy(), (xb.shape[2],xb.shape[3]))
+            # save ground truth depths to list
+            gt_depths[count_test_batches][i] = yb[i].cpu().detach().numpy()
+        count_test_batches += 1
+    return numpy_depth_prediction, gt_depths
+
+def mean_and_std_errors(predictions, grnd_trth, val_size, batch_size):
+
+    error_dictionary = {}
+    chunk_size = val_size//batch_size
+    for i in range(chunk_size+1):
+        error_dictionary[f"{i}"] = {}
+    for i in range(chunk_size+1):
+        for j in range(batch_size):
+            error_dictionary[f"{i}"][f"{j}"] = {}
+
+    #//////////////////////////////////////#
+
+    # calculate the errors
+
+    for i in range(chunk_size-1):
+        for j in range(batch_size-1):
+            error_dictionary[f"{i}"][f"{j}"] = calc_errors(predictions[i][j], grnd_trth[i][j])
+        #print(f"Calculating errors for batch {i} of {int(chunk_size)}")
+
+    #//////////////////////////////////////#
+
+    # initialisation of average errors
+    difference_err_avg  = 0
+    sqr_diff_err_avg    = 0
+    inv_err_avg         = 0
+    inv_sqr_err_avg     = 0
+    log_err_avg         = 0
+    log_sqr_err_avg     = 0
+    log_non_abs_err_avg = 0
+    abs_rel_err_avg     = 0
+    sqr_rel_err_avg     = 0
+
+    #//////////////////////////////////////#
+
+    # increment the errors
+
+    for i in range(0, chunk_size):
+        for j in range(0, batch_size-1):
+            difference_err_avg  += error_dictionary[f"{i}"][f"{j}"][0]
+            sqr_diff_err_avg    += error_dictionary[f"{i}"][f"{j}"][1]
+            inv_err_avg         += error_dictionary[f"{i}"][f"{j}"][2]
+            inv_sqr_err_avg     += error_dictionary[f"{i}"][f"{j}"][3]
+            log_err_avg         += error_dictionary[f"{i}"][f"{j}"][4]
+            log_sqr_err_avg     += error_dictionary[f"{i}"][f"{j}"][5]
+            log_non_abs_err_avg += error_dictionary[f"{i}"][f"{j}"][6]
+            abs_rel_err_avg     += error_dictionary[f"{i}"][f"{j}"][7]
+            sqr_rel_err_avg     += error_dictionary[f"{i}"][f"{j}"][8]
+
+    #//////////////////////////////////////#
+
+    ## divide by number of images to get average error
+    difference_err_avg  /= (val_size)
+    sqr_diff_err_avg    /= (val_size)
+    inv_err_avg         /= (val_size)
+    inv_sqr_err_avg     /= (val_size)
+    log_err_avg         /= (val_size)
+    log_sqr_err_avg     /= (val_size)
+    log_non_abs_err_avg /= (val_size)
+    abs_rel_err_avg     /= (val_size)
+    sqr_rel_err_avg     /= (val_size)
+
+    #//////////////////////////////////////#
+
+    # initialise difference counters
+    difference_err_count    = 0
+    sqr_diff_err_count      = 0
+    inv_err_count           = 0
+    inv_sqr_err_count       = 0
+    log_err_count           = 0
+    log_sqr_err_count       = 0
+    log_non_abs_err_count   = 0
+    abs_rel_err_count       = 0
+    sqr_rel_err_count       = 0
+
+    #//////////////////////////////////////#
+
+    # sum squared differences
+    for i in range(0, chunk_size):
+        for j in range(0, batch_size-1):
+            difference_err_count    += (error_dictionary[f"{i}"][f"{j}"][0] - difference_err_avg)**2
+            sqr_diff_err_count      += (error_dictionary[f"{i}"][f"{j}"][0] - sqr_diff_err_avg)**2
+            inv_err_count           += (error_dictionary[f"{i}"][f"{j}"][0] - inv_err_avg)**2
+            inv_sqr_err_count       += (error_dictionary[f"{i}"][f"{j}"][0] - inv_sqr_err_avg)**2
+            log_err_count           += (error_dictionary[f"{i}"][f"{j}"][0] - log_err_avg)**2
+            log_sqr_err_count       += (error_dictionary[f"{i}"][f"{j}"][0] - log_sqr_err_avg)**2
+            log_non_abs_err_count   += (error_dictionary[f"{i}"][f"{j}"][0] - log_non_abs_err_avg)**2
+            abs_rel_err_count       += (error_dictionary[f"{i}"][f"{j}"][0] - abs_rel_err_avg)**2
+            sqr_rel_err_count       += (error_dictionary[f"{i}"][f"{j}"][0] - sqr_rel_err_avg)**2
+
+    #//////////////////////////////////////#
+
+    # divide by number of test images
+    difference_err_count    /= val_size
+    sqr_diff_err_count      /= val_size
+    inv_err_count           /= val_size
+    inv_sqr_err_count       /= val_size
+    log_err_count           /= val_size
+    log_sqr_err_count       /= val_size
+    log_non_abs_err_count   /= val_size
+    abs_rel_err_count       /= val_size
+    sqr_rel_err_count       /= val_size
+
+    #//////////////////////////////////////#
+
+    # square root
+    difference_err_sigma    = math.sqrt(difference_err_count)
+    sqr_diff_err_sigma      = math.sqrt(sqr_diff_err_count)
+    inv_err_sigma           = math.sqrt(inv_err_count)
+    inv_sqr_err_sigma       = math.sqrt(inv_sqr_err_count)
+    log_err_sigma           = math.sqrt(log_err_count)
+    log_sqr_err_sigma       = math.sqrt(log_sqr_err_count)
+    log_non_abs_err_sigma   = math.sqrt(log_non_abs_err_count)
+    abs_rel_err_sigma       = math.sqrt(abs_rel_err_count)
+    sqr_rel_err_sigma       = math.sqrt(sqr_rel_err_count)
+
+    #//////////////////////////////////////#
+
+    # collect and return errors and std deviations
+    mean_errors = [difference_err_avg, sqr_diff_err_avg, inv_err_avg, inv_sqr_err_avg, log_err_avg, log_sqr_err_avg, log_non_abs_err_avg, abs_rel_err_avg, sqr_rel_err_avg]
+    std_devs    = [difference_err_sigma, sqr_diff_err_sigma, inv_err_sigma, inv_sqr_err_sigma, log_err_sigma, log_sqr_err_sigma, log_non_abs_err_sigma, abs_rel_err_sigma, sqr_rel_err_sigma]
+    return mean_errors, std_devs
